e5bfa6dffba034dc526db4ee667b62636f3a052f
MarySheng <mdiosgalla@gmail.com>
2020-03-27 15:13:04 +0800

    Initialize commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+  import React from 'react';
+  import ReactDOM from 'react-dom';
+   import faker from 'faker';
+   import CommentDetail from './CommentDetail';
+   import ApprovalCard from './ApprovalCard';
+  
+const App = () => {
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+            <CommentDetail 
+             writer= "Mary"
+             time="Yesterday at 10:00PM" 
+             text="Very nice blog"
+             img={faker.image.avatar()}/>
+             </ApprovalCard>
+            
+            <ApprovalCard>
+            <CommentDetail
+             writer="Ting" 
+             time="Today at 9:00AM"
+             text="Great adventure"
+             img={faker.image.avatar()}/>
+             </ApprovalCard>
+             
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+             
+            <ApprovalCard>
+            <CommentDetail 
+             writer="Sheng"
+             time="Today at 5:00PM"
+             text="Happy memories"
+             img={faker.image.avatar()}/>
+             </ApprovalCard>
+             
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+             
+             <ApprovalCard>
+            <CommentDetail 
+             writer="Jerk"
+             time="Today at 12:00PM"
+             text="Just so so"
+             img={faker.image.avatar()}/>
+             </ApprovalCard>
+            
+            </div>
+        
+    );
+};
+
+
+ReactDOM.render(<App  />, document.querySelector('#root'));
